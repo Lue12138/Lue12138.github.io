4695b40b94c3eb24cacc639de081246fa9538ec4
Lue12138 <shawlue2@gmail.com>
2023-08-30 12:04:11 -0400

    version 2.0

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let slideIndex = 1;
-    showSlides(slideIndex);
+// let slideIndex = 1;
+//     showSlides(slideIndex);
     
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
+//     function plusSlides(n) {
+//         showSlides(slideIndex += n);
+//     }
     
-    function showSlides(n, no) {
-        let i;
-        let slides = document.getElementsByClassName("mySlides1");
-        if (n > slides.length) {slideIndex = 1}    
-        if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";  
-        }
-        slides[slideIndex-1].style.display = "block";  
-    }
+//     function showSlides(n, no) {
+//         let i;
+//         let slides = document.getElementsByClassName("mySlides1");
+//         if (n > slides.length) {slideIndex = 1}    
+//         if (n < 1) {slideIndex = slides.length}
+//         for (i = 0; i < slides.length; i++) {
+//             slides[i].style.display = "none";  
+//         }
+//         slides[slideIndex-1].style.display = "block";  
+//     }
